15fc3dd61dd7eb19190252034dfd124a277429f1
Dlayane-Mat <matjiladlayane@gmail.com>
2024-11-14 23:40:26 +0200

    added the weather component and the content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      <h1>Weather Application</h1>
+      <Weather />
+      <footer>
+        <p>
+          This project was coded{" "}
+          <a href="https://github.com/Dlayane-Mat/" target="blank">
+            Dlayane Matjila
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/Dlayane-Mat/my-weather-application"
+            target="blank"
+          >
+            GitHub
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
---
+++ src/Weather.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather() {
+  let [city, setCity] = useState("");
+  let [message, setMessage] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  function currentCity(event) {
+    setCity(event.target.value);
+  }
+
+  function showWeather(response) {
+    setMessage(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "8402ccd9e55983fce71eeeaa1d2bd1fc";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city"
+        autofocus
+        required
+        onChange={currentCity}
+      />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (message) {
+    return (
+      <div>
+        {form}
+        <h2 className="City">{city}</h2>
+        <p>
+          Temperature: {Math.round(weather.temperature)}°C
+          <br />
+          Description: {weather.description}
+          <br />
+          Humidity: {weather.humidity}%<br />
+          Wind: {weather.wind}km/h
+          <br />
+          <img src={weather.icon} alt={weather.description} />
+          <br />
+        </p>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
