7a1ebec5f9279b74c3ff1b6004449bbe6a1dd06d
Dlayane-Mat <matjiladlayane@gmail.com>
2024-11-19 08:30:33 +0200

    added the API intergration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="South Africa" />
         <footer>
           <p>
             This project was coded{" "}
--- src/Search.js
+++ src/Search.js
@@ -1,50 +1,76 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
+
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="form-control"
-              autoFocus="on"
-            />
+  const [forecast, setForecast] = useState({ ready: false });
+  function handleResponse(response) {
+    setForecast({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      date: "Monday 20:15",
+      iconUrl: "https://ss1.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    });
+  }
+  if (forecast.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1>Limpopo</h1>
-      <ul>
-        <li>Monday 20:15</li>
-        <li>Clear Sky</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ss1.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="cloudy picture"
-              className="floate-left"
-            />
-            <span className="temperature">21</span>
-            <span className="unit">"°C</span>
-          </div>
+        </form>
+        <h1>{forecast.city}</h1>
+        <ul>
+          <li>{forecast.date}</li>
+          <li className="text-capitalize">{forecast.description}</li>
+        </ul>
+        <div className="row mt-3">
           <div className="col-6">
-            <ul>
-              <li>Temperature : 21°C</li>
-              <li>Humidity : 56%</li>
-              <li>Wind : 3.06km/h</li>
-            </ul>
+            <div className="clearfix">
+              <img
+                src={forecast.iconUrl}
+                alt={forecast.description}
+                className="floate-left"
+              />
+              <span className="temperature">
+                {Math.round(forecast.temperature)}
+              </span>
+              <span className="unit">"°C</span>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Humidity : {forecast.humidity}%</li>
+                <li>Wind :{forecast.wind}km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "0bd4693b39a661e4b0370fe772t7a9o3";
+    let city = "Limpopo";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading..";
+  }
 }
